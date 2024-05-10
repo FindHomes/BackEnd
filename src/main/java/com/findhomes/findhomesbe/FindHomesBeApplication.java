efbb952947402aa5a19ace5e63556cda5bf52f6e
DingWoonee <slm0523@naver.com>
2024-05-10 23:50:58 +0900

    first commit

---
+++ src/main/java/com/findhomes/findhomesbe/FindHomesBeApplication.java
@@ -0,0 +1,13 @@
+package com.findhomes.findhomesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindHomesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindHomesBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findhomes/findhomesbe/FindHomesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findhomes.findhomesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindHomesBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
