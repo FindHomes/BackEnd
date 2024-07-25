e938f68ef1cc19df7b25d9ba2018381bd7d0270f
DingWoonee <slm0523@naver.com>
2024-07-25 11:01:26 +0900

    fix: House 외부 연결 리스트 JsonIgnore

--- src/main/java/com/findhomes/findhomesbe/config/SwaggerConfig.java
+++ src/main/java/com/findhomes/findhomesbe/config/SwaggerConfig.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     private static final String API_NAME = "Kustaurant Mobile Application API";
-    private static final String API_VERSION = "v1.0.1";
+    private static final String API_VERSION = "v1.0.0";
     private static final String API_DESCRIPTION = """
     찾아줘!홈즈 모바일 앱 API 문서입니다.
 
-    **Version 1.0.0 (2024-07-20)**
+    **Version 1.0.0 (2024-07-25)**
     - 매물 검색을 위한 조건 입력 API 구성
     """;
 
