52ad148adb771262e4527c3793bf0b02b983c3d2
imjaehyeong <dlawogud517@gmail.com>
2024-08-29 17:49:30 +0900

    feat:cors 오류 해결 시도

--- src/main/java/com/findhomes/findhomesbe/config/WebConfig.java
+++ src/main/java/com/findhomes/findhomesbe/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.findhomes.findhomesbe.config;
 import com.findhomes.findhomesbe.argument_resolver.SessionValueArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,5 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new SessionValueArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 모든 도메인 허용, 필요에 따라 특정 도메인으로 제한 가능
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
