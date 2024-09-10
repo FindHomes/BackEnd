747196556c1cf6c340e754408f471902d4ccf17c
imjaehyeong <dlawogud517@gmail.com>
2024-09-10 17:55:22 +0900

    fix:캐스팅 버그 생겨서 브랜치 바꿔서 롤백해서 공간인덱싱 진행

--- src/main/java/com/findhomes/findhomesbe/condition/domain/FacilityCategory.java
+++ src/main/java/com/findhomes/findhomesbe/condition/domain/FacilityCategory.java
@@ -3,94 +3,111 @@ package com.findhomes.findhomesbe.condition.domain;
 import com.findhomes.findhomesbe.entity.Industry;
 import com.findhomes.findhomesbe.repository.industry.*;
 import lombok.Getter;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
-/**
- * 각 FacilityCategory enum 객체는 repository 빈 이름과 repository에서 데이터를 가져오는 람다식을 필드로 가짐.
- * repository를 여기서 조회하는걸 생각했는데, enum의 라이프사이클상 repository 빈 보다 먼저 생성돼서 여기서 바로 참조가 불가능함.
- * 그래서 람다식을 호출할 때 repository bean 객체를 넣어주는 방식으로 했음...
- * 이게 처음에 여기서 repository를 가져오는 걸로 생각했던거라 좀 이상할 수 있는데 수정하면 좋긴할듯... 언젠가..
- */
 @Getter
 public enum FacilityCategory {
 
     동물병원("animalHospitalIndustryRepository",
-            (repository, detailName) ->
-                    ((AnimalHospitalIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                AnimalHospitalIndustryRepository repository = applicationContext.getBean("animalHospitalIndustryRepository", AnimalHospitalIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     베이커리("bakeryIndustryRepository",
-            (repository, detailName) ->
-                    ((BakeryIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                BakeryIndustryRepository repository = applicationContext.getBean("bakeryIndustryRepository", BakeryIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     목욕탕("bathhouseIndustryRepository",
-            (repository, detailName) ->
-                    ((BathhouseIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                BathhouseIndustryRepository repository = applicationContext.getBean("bathhouseIndustryRepository", BathhouseIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     미용실("beautyIndustryRepository",
-            (repository, detailName) ->
-                    ((BeautyIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                BeautyIndustryRepository repository = applicationContext.getBean("beautyIndustryRepository", BeautyIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     영화관("cinemaIndustryRepository",
-            (repository, detailName) ->
-                    ((CinemaIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                CinemaIndustryRepository repository = applicationContext.getBean("cinemaIndustryRepository", CinemaIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     병원("clinicIndustryRepository",
-            (repository, detailName) ->
-                    ((ClinicIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                ClinicIndustryRepository repository = applicationContext.getBean("clinicIndustryRepository", ClinicIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     공연장("concertHallIndustryRepository",
-            (repository, detailName) ->
-                    ((ConcertHallIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                ConcertHallIndustryRepository repository = applicationContext.getBean("concertHallIndustryRepository", ConcertHallIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     피시방("gameIndustryRepository",
-            (repository, detailName) ->
-                    ((GameIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                GameIndustryRepository repository = applicationContext.getBean("gameIndustryRepository", GameIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     헬스장("gymIndustryRepository",
-            (repository, detailName) ->
-                    ((GymIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                GymIndustryRepository repository = applicationContext.getBean("gymIndustryRepository", GymIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     종합병원("hospitalIndustryRepository",
-            (repository, detailName) ->
-                    ((HospitalIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                HospitalIndustryRepository repository = applicationContext.getBean("hospitalIndustryRepository", HospitalIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     노래방("karaokeIndustryRepository",
-            (repository, detailName) ->
-                    ((KaraokeIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                KaraokeIndustryRepository repository = applicationContext.getBean("karaokeIndustryRepository", KaraokeIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     약국("pharmacyIndustryRepository",
-            (repository, detailName) ->
-                    ((PharmacyIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                PharmacyIndustryRepository repository = applicationContext.getBean("pharmacyIndustryRepository", PharmacyIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     ),
     음식점("restaurantIndustryRepository",
-            (repository, detailName) ->
-                    ((RestaurantIndustryRepository) repository).findByDetailName(detailName)
+            (applicationContext, detailName) -> {
+                RestaurantIndustryRepository repository = applicationContext.getBean("restaurantIndustryRepository", RestaurantIndustryRepository.class);
+                return (List<Industry>) repository.findByDetailName(detailName);
+            }
     );
 
     private final String repositoryBeanName;
-    private final BiFunction<JpaRepository<? extends Industry, Integer>, String, List<? extends Industry>> getIndustryListWhenNotAllFunction;
+    private final BiFunction<ApplicationContext, String, List<Industry>> getIndustryListWhenNotAllFunction;
 
     FacilityCategory(String repositoryBeanName,
-                     BiFunction<JpaRepository<? extends Industry, Integer>, String, List<? extends Industry>> getIndustryListWhenNotAllFunction) {
+                     BiFunction<ApplicationContext, String, List<Industry>> getIndustryListWhenNotAllFunction) {
         this.repositoryBeanName = repositoryBeanName;
         this.getIndustryListWhenNotAllFunction = getIndustryListWhenNotAllFunction;
     }
 
-    // detailName이 "all"이 아닐 경우 호출해서 Industry 리스트를 반환받는 함수.
-    public <T extends Industry> List<Industry> getIndustryListWhenNotAll(JpaRepository<T, Integer> repository, String detailName) {
-        return (List<Industry>) getIndustryListWhenNotAllFunction.apply(repository, detailName);
+    public List<Industry> getIndustryListWhenNotAll(ApplicationContext applicationContext, String detailName) {
+        return getIndustryListWhenNotAllFunction.apply(applicationContext, detailName);
     }
 
-    // 보유 데이터 목록 문자열로 반환
     public static String getAllData() {
         return Arrays.stream(FacilityCategory.values())
                 .map(Enum::name)
                 .collect(Collectors.joining(", "));
     }
-
 }
